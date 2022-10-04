dcfb260351b2e85cbdb6a662a8500cc0e5fa03cc
LuizProgrammer <luizmartinzoliveira@gmail.com>
2022-10-04 10:31:20 -0300

    Adicionado mecanismo de busca dos dados de usuario, através do id armazenado em localStorage

--- FRONT-END/script/conteudo.js
+++ FRONT-END/script/conteudo.js
@@ -2,12 +2,37 @@ class Conteudo {
   constructor() {
 }
 
-nomeUsuario(){
-  let user = JSON.parse(localStorage.getItem("user"));
-  let nome = user._name;
+dadosUsuario(){
+  let id = JSON.parse(localStorage.getItem("user"));
+  //let nome = user._name;
+  let user = new XMLHttpRequest();
+  user.onload = function() {
+    let objuser = JSON.parse(user.response);
+    let userlog = new Usuario(objuser.id, objuser.name, objuser.email, objuser.phone, objuser.username, objuser.address, objuser.company);
+    let tgName = document.querySelector(".userName");
+    tgName.innerText = userlog.getName();
+  }
+  user.open("GET","https://jsonplaceholder.typicode.com/users/"+id);
+  user.send();
 
-  let tgName = document.querySelector(".userName");
-  tgName.innerText = nome;
+}
+
+historico(historico){
+
+    let tbody = document.getElementById('tbody');
+
+    for(let i = 0; i < historico.length; i++){
+
+        let linha = tbody.insertRow();
+
+        let dta = linha.insertCell();
+        let items = linha.insertCell();
+        let valor = linha.insertCell();
+
+        dta.innerText = historico[i].quantidade;
+        items.innerText = historico[i].item;
+        valor.innerText = historico[i].valor;
+    }
 }
 
 maisVendidos(){
@@ -54,4 +79,4 @@ maisVendidos(){
 }
 
 let ct = new Conteudo();
-ct.nomeUsuario();
+ct.dadosUsuario();
--- FRONT-END/script/login.js
+++ FRONT-END/script/login.js
@@ -1,18 +1,19 @@
 class Login{
     constructor(){
 
-
     }
 
 
 
     logar(){
-        if(this.validaCampos()) {
-            if(this.comparaCampos()) {
-              //window.location.href = "usuario.html";
-              window.alert("deu certo");
-            }
-        }
+      let form = document.querySelector(".form");
+      form.addEventListener("submit",function(event) {
+        event.preventDefault();
+      });
+
+      if(this.validaCampos()) {
+        this.comparaCampos();
+      }
     }
     validaCampos(){
 
@@ -44,32 +45,32 @@ class Login{
         //convertendo JSON com array
         let usr = new XMLHttpRequest();
 
+        var status = "procurando";
+
         usr.onload = function() {
           let logJson = JSON.parse(usr.response);
-          console.log(usr);
+
               for(let i = 0; i < logJson.length; i++){
 
                   //armazenando dados percorridos da array para comparação
                   let nomeJson = logJson[i].name;
                   let senhaJson = logJson[i].username;
 
-                  let status = "procurando";
-
                   //armazenando objeto da Array compatível no localStorage
                   if(nome == nomeJson && senha == senhaJson){
 
                       let usuario = new Usuario(logJson[i].id,logJson[i].name,logJson[i].email,logJson[i].phone,logJson[i].username,logJson[i].address,logJson[i].company);
-                      localStorage.setItem("user", JSON.stringify(usuario));
+                      localStorage.setItem("user", JSON.stringify(usuario.getId()));
                       status = "encontrado";
-                      return true;
+                      location.href = "usuario.html";
                   }
-                  if(status == "encontrado") {
+                  if(this.status == "encontrado") {
 
                       i = logJson.length;
                   }
                   if(i == logJson.length - 1 && status != "encontrado") {
                       window.alert("nenhum usuário encontrado");
-                      return false;
+
                   }
               }
         }
@@ -78,25 +79,6 @@ class Login{
 
         usr.send();
 
-
-    }
-
-    historico(historico){
-
-        let tbody = document.getElementById('tbody');
-
-        for(let i = 0; i < historico.length; i++){
-
-            let linha = tbody.insertRow();
-
-            let dta = linha.insertCell();
-            let items = linha.insertCell();
-            let valor = linha.insertCell();
-
-            dta.innerText = historico[i].quantidade;
-            items.innerText = historico[i].item;
-            valor.innerText = historico[i].valor;
-        }
     }
 
 
