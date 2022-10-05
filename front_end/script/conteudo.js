39dc024844658b4b3e6293f0f61f5b5da667b932
LuizProgrammer <luizmartinzoliveira@gmail.com>
2022-10-05 17:10:17 -0300

    adicionando area de produtos o index

--- front_end/script/conteudo.js
+++ front_end/script/conteudo.js
@@ -16,7 +16,6 @@ userDatas(){
 
     ct.userName(userlog.getUsername());
     ct.historico(userlog.getCompany());
-    console.log(userlog.getCompany());
   }
   user.open("GET","https://jsonplaceholder.typicode.com/users/"+id);
   user.send();
@@ -45,38 +44,48 @@ historico(historico){
         valor.innerText = historico.name;
     //}
 }
+produtos(){
+  this.populares();
+  this.novos();
+  this.destaques();
+}
 
-maisVendidos(){
+populares(){
 
   let mv = new XMLHttpRequest();
 
   mv.onload = function() {
 
-    let itens = JSON.parse(usr.response);
+    let produtos = JSON.parse(mv.response);
 
-    for(let i = 0; i < itens.length; i++){
+    for(let i = 0; i < 10; i++){
 
       //abaixo conteúdo dinâmico
-      const card = document.createElement("div");
-      card.id = "card";
+      let item = document.createElement("div");
+      item.className = "item";
 
-      const img = document.createElement("div");
-      const descricao = document.createElement("p");
-      const valor = document.createElement("cite");
+      let img = document.createElement("img");
+      let info = document.createElement("p");
+      let valor = document.createElement("span");
+
+      img.className = "imagem_item";
+      info.className = "info";
+      valor.className = "valor";
 
       //inserindo valores JSON
-      img.innerText = itens[i].id ;
-      descricao.innerText = itens[i].title ;
-      valor.innerText = itens[i].url ;
+      img.src = produtos[i].url ;
+      info.innerText = produtos[i].title ;
+      valor.innerText = "R$ " + produtos[i].id + "0,00";
       //inserindo valores JSON
 
-      card.appendChild(img);
-      card.appendChild(descricao);
-      card.appendChild(valor);
+      item.appendChild(img);
+      item.appendChild(info);
+      item.appendChild(valor);
 
-      const main = document.getElementById("vendidos");
+      let populares = document.querySelector(".itens_populares");
 
-      main.appendChild(card);
+      populares.appendChild(item);
+
       //acima conteúdo dinâmico
     }
   }
@@ -86,6 +95,99 @@ maisVendidos(){
   mv.send();
 
  }
+
+novos(){
+
+   let mv = new XMLHttpRequest();
+
+   mv.onload = function() {
+
+     let produtos = JSON.parse(mv.response);
+
+     for(let i = 0; i < 10; i++){
+
+       //abaixo conteúdo dinâmico
+       let item = document.createElement("div");
+       item.className = "item";
+
+       let img = document.createElement("img");
+       let info = document.createElement("p");
+       let valor = document.createElement("span");
+
+       img.className = "imagem_item";
+       info.className = "info";
+       valor.className = "valor";
+
+       //inserindo valores JSON
+       img.src = produtos[i].url ;
+       info.innerText = produtos[i].title ;
+       valor.innerText = "R$ " + produtos[i].id + "0,00";
+       //inserindo valores JSON
+
+       item.appendChild(img);
+       item.appendChild(info);
+       item.appendChild(valor);
+
+       let novos = document.querySelector(".itens_novos");
+
+       novos.appendChild(item);
+
+       //acima conteúdo dinâmico
+     }
+   }
+
+   mv.open("GET","https://jsonplaceholder.typicode.com/todos/1/photos");
+
+   mv.send();
+
+  }
+
+destaques(){
+
+    let mv = new XMLHttpRequest();
+
+    mv.onload = function() {
+
+      let produtos = JSON.parse(mv.response);
+
+      for(let i = 0; i < 10; i++){
+
+        //abaixo conteúdo dinâmico
+        let item = document.createElement("div");
+        item.className = "item";
+
+        let img = document.createElement("img");
+        let info = document.createElement("p");
+        let valor = document.createElement("span");
+
+        img.className = "imagem_item";
+        info.className = "info";
+        valor.className = "valor";
+
+        //inserindo valores JSON
+        img.src = produtos[i].url ;
+        info.innerText = produtos[i].title ;
+        valor.innerText = "R$ " + produtos[i].id + "0,00";
+        //inserindo valores JSON
+
+        item.appendChild(img);
+        item.appendChild(info);
+        item.appendChild(valor);
+
+        let destaques = document.querySelector(".itens_destaques");
+
+        destaques.appendChild(item);
+
+        //acima conteúdo dinâmico
+      }
+    }
+
+    mv.open("GET","https://jsonplaceholder.typicode.com/todos/1/photos");
+
+    mv.send();
+
+   }
 }
 
 let ct = new Conteudo();
+ct.produtos();
