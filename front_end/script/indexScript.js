b1bc3235f0087e5b9c9987d2d528888c9e4fb3fd
LuizProgrammer <luizmartinzoliveira@gmail.com>
2022-10-11 12:20:42 -0300

    implementando responsividade

--- front_end/script/indexScript.js
+++ front_end/script/indexScript.js
@@ -3,6 +3,26 @@ class Conteudo {
     this.userDatas();
 }
 
+menu(action){
+  let menu = document.querySelector(".menu");
+  let menUser = document.querySelector(".menu_user");
+
+  let btop = document.querySelector(".menu_op");
+
+  if (action == "open") {
+    btop.style.display = "none";
+
+    menu.style.display = "flex";
+    menUser.style.display = "flex";
+  }
+  if (action == "close") {
+    btop.style.display = "block";
+
+    menu.style.display = "none";
+    menUser.style.display = "none";
+  }
+}
+
 userDatas(){
 
 
